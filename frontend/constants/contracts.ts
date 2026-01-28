9e67b7c6f7a494684991cd14dfd03f7e8efb8aae
Ajidokwu Sabo <realjaiboi70@gmail.com>
2026-01-28 20:50:42 +0100

    feat: update config

--- frontend/constants/contracts.ts
+++ frontend/constants/contracts.ts
@@ -9,7 +9,7 @@ export const REWARD_CONTRACT_ADDRESSES: Record<number, Address | undefined> = {
   [polygon.id]: process.env.NEXT_PUBLIC_POLYGON_REWARD as Address,
 }
 export const TYC_TOKEN_ADDRESS: Record<number, Address | undefined> = {
-  [polygon.id]: process.env.NEXT_PUBLIC_POLYGON_REWARD as Address,
+  [polygon.id]: process.env.NEXT_PUBLIC_POLYGON_TOKEN as Address,
   }
 
 export const USDC_TOKEN_ADDRESS: Record<number, Address | undefined> = {
